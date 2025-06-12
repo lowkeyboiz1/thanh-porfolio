06ac8bce2d3f429aa1fe84500672f441bf988d4a
lowkeyboiz1 <khangluong20021@gmail.com>
2025-06-12 09:34:27 +0700

    remove WorkExperience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
       </div>
       <Education />
       <MySkills />
-      <WorkExperience />
+      {/* <WorkExperience /> */}
       <Projects />
       <Contact />
       <Footer />
